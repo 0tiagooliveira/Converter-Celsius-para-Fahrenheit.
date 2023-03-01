4fdd59018c938b267381d5b132fc40b81ae8a3ac
0tiagooliveira <123430824+0tiagooliveira@users.noreply.github.com>
2023-03-01 16:42:18 -0300

    Create Converter Celsius para Fahrenheit.py

---
+++ Converter Celsius para Fahrenheit.py
@@ -0,0 +1,3 @@
+temperatura = float(input('Inform a temperatura em ºC: '))
+resultado = (temperatura * 9/5)+32
+print('A temperatura de {:.1f}ºC coresponde a {:.1f}ºF!'.format(temperatura, resultado))
